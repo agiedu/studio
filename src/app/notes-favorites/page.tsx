b2c8a5d1a6c434a23204a4a3b871a1d43709b11e
le y <laotouerle@gmail.com>
2025-07-25 08:27:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.label c

--- src/app/notes-favorites/page.tsx
+++ src/app/notes-favorites/page.tsx
@@ -376,7 +376,7 @@ function NotesFavoritesPageContent() {
                         </div>
                         <div className="flex items-center space-x-2">
                           <RadioGroupItem value="sequential" id="mode-sequential" />
-                          <Label htmlFor="mode-sequential" className="flex items-center gap-1 cursor-pointer"><ListOrdered className="h-4 w-4"/>列表循环播放模式</Label>
+                          <Label htmlFor="mode-sequential" className="flex items-center gap-1 cursor-pointer"><ListOrdered className="h-4 w-4"/>List loop mode</Label>
                         </div>
                       </RadioGroup>
                   </div>
